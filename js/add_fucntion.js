4913572329076bec28714ceeab9881ef192940de
manii5228 <manimanjunath.v@gmail.com>
2026-01-28 11:33:41 +0530

    fav function and the printing from the var

--- js/add_fucntion.js
+++ js/add_fucntion.js
@@ -1,8 +1,10 @@
-var a=10;
-var b=10;
+var a=10
+var b=10
 
 function add(){
-    var c =a+b;
-    console.log(c);
+    var c =a+b
+    console.log(c)
 }
 add()
+
+// ; is not some inp in the java script
---
+++ js/fav.js
@@ -0,0 +1,10 @@
+var Factor="RDJ"
+var Fplayer="Ronload Weasly"
+Fmovie="chhichore"
+
+function favourite(){
+    console.log("My Fav Actor is:"+Factor)
+    console.log("My Fav Player is:",Fplayer)
+    console.log("My Fav MOive:",Fmovie)
+}
+favourite()
