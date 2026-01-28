28d408f644c3e5c00c7c62b750c1a729cc6938f0
manii5228 <manimanjunath.v@gmail.com>
2026-01-28 11:07:14 +0530

    unit 1 html part

---
+++ js/sample.js
@@ -0,0 +1,28 @@
+price =500
+product="apple"
+tax=18
+console.log(product)
+ptax=(price*18)/100
+console.log(ptax)
+
+
+FriutName="apple"
+count=250
+Price=15
+total=count*price
+console.log(FriutName,total)
+
+var a= 10
+console.log("a:",a)
+var b=a++
+console.log("b:",b)
+console.log("a:",a)
+var c=++a
+console.log("c:",c)
+console.log("a:",a)
+var d=a--
+console.log("d:",d)
+console.log("a:",a)
+var e=--a
+console.log("e:",e)
+console.log("a:",a)
